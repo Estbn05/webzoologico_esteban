e15532a23a3e145c69e4b757db7c2678e75f3e42
Estbn05 <yefrye.avilaz@konradlorenz.edu.co>
2025-10-30 21:28:09 -0500

    hasta la instalacion de bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
 import { AnimalComponent } from './components/animal-component/animal-component';
 
-export const routes: Routes = [{path:"animal",component:AnimalComponent}];
+export const routes: Routes = [ //rutas de la aplicacion de angular no de la api de express
+    {path: 'animales', component: AnimalComponent}
+];
--- src/app/components/animal-component/animal-component.ts
+++ src/app/components/animal-component/animal-component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AnimalService } from '../../services/animal-service';
 
 @Component({
   selector: 'app-animal-component',
@@ -8,5 +9,16 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './animal-component.css',
 })
 export class AnimalComponent {
-  titulo="mi titulo"
+  animalList: any = [];
+
+  constructor(private animalService: AnimalService) { }
+
+  getAllAnimals() { //se puede cambiar nombre si no gusta
+    this.animalService.getAllAnimalsData().subscribe((data: {}) => { //toma el objeto del servicio y hace el metodo del servicio/ en data se guarda lo que devuelva el servicio
+      this.animalList = data;
+    });
+  }
+  ngOnInit() {  //caundo se haga un llamado a este componente, llama al metodo getAllAnimals
+    this.getAllAnimals();
+  }
 }
