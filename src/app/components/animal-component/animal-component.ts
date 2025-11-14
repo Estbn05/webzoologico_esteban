761bc0cf1f8428555edf72de88f06d91d0f19205
usuario <correo@sincorreo.com>
2025-11-13 20:32:09 -0500

    ultima presentacion

--- src/app/components/animal-component/animal-component.ts
+++ src/app/components/animal-component/animal-component.ts
@@ -4,16 +4,21 @@ import { AnimalService } from '../../services/animal-service';
 import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-animal-component',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './animal-component.html',
   styleUrl: './animal-component.css',
 })
 export class AnimalComponent {
   animalList: any = [];
   animalForm: FormGroup | any;
+  idAnimal: any;
+ editableAnimal: boolean = false;
+
 
   constructor(private animalService: AnimalService,
     private toastr: ToastrService,
@@ -51,5 +56,47 @@ export class AnimalComponent {
       }
     );
   }
+   updateAnimalEntry() {
+    //Removiendo valores vacios del formulario de actualización
+    for (let key in this.animalForm.value) {
+      if (this.animalForm.value[key] === '') {
+        this.animalForm.removeControl(key);
+      }
+    }
+    this.animalService.updateAnimal(this.idAnimal, this.animalForm.value).subscribe(
+      () => {
+        //Enviando mensaje de confirmación
+        this.newMessage("Animal editado");
+      }
+    );
+  }
+   toggleEditAnimal(id: any) {
+    this.idAnimal = id;
+    console.log(this.idAnimal)
+     this.editableAnimal = !this.editableAnimal;
+    console.log(this.editableAnimal)
+    this.animalService.getOneAnimal(id).subscribe(
+      data => {
+        this.animalForm.setValue({
+          nombre: data.nombre,
+          edad: data.edad,
+          tipo: data.tipo,          
+        });
+      }
+    );
+   
+  }
+   deleteAnimalEntry(id: any) {
+    console.log(id)
+    this.animalService.deleteAnimal(id).subscribe(
+      () => {
+        //Enviando mensaje de confirmación
+        this.newMessage("Animal eliminado");
+      }
+    );
+  }
+
+
+
 
 }
